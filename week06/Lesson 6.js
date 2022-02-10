e92e479d358d041380b4f02007e31fd07c42548d
xphile1998 <77454377+xphile1998@users.noreply.github.com>
2022-02-10 13:46:44 -0800

    Create Lesson 6.js

---
+++ week06/Lesson 6.js
@@ -0,0 +1,24 @@
+// with .forEach
+const foodsElement = document.querySelector('#favorite-foods');
+function createandAppendFoodItem(food) {
+  let favoriteFood = document.createElement('li');
+  favoriteFood.textContent = food;
+  foodsElement.appendChild(favoriteFood);
+}
+myInfo.favoriteFoods.forEach(createAndAppendFoodItem);
+}
+
+// with .map
+  const foodsElement = document.querySelector('#favorite-foods');
+  function mapFoodItem(food) {
+    let favoriteFood = document.createElement('li');
+    favoriteFood.textContent = food;
+    return favoriteFood;
+  }
+  // this function could also be written this way using a template literal:
+  function mapFoodItemSmall(food) {
+    return `<li>${food}</li>`;
+  }
+  const foodListElements = myInfo.favoriteFoods.map(mapFoodItem);
+  // we need to flatten the array of strings into one big string. .join does this.
+  foodsElement.innerHTML = foodListElements.join('');
