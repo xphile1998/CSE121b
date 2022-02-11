d287fa26165222879c3f4bf87b591e44c57c76d5
xphile1998 <77454377+xphile1998@users.noreply.github.com>
2022-02-10 16:51:15 -0800

    updating folders

--- week06/Lesson 6.js
+++ week06/Lesson 6.js
@@ -22,3 +22,57 @@ myInfo.favoriteFoods.forEach(createAndAppendFoodItem);
   const foodListElements = myInfo.favoriteFoods.map(mapFoodItem);
   // we need to flatten the array of strings into one big string. .join does this.
   foodsElement.innerHTML = foodListElements.join('');
+
+  // OR we could in fact simplify this down to one line!
+document.querySelector("#favorite-foods").innerHTML = myInfo.favoriteFoods.map((food) => `<li>${food}</li>`).join("");
+
+// =============================================================================
+// reusable!
+myInfo = {
+  name: "Brother T",
+  photo: "images/photo.jpg",
+  favoriteFoods: ["Fettucini", "Steak", "Chicken", "Shrimp", "Baked Potato"],
+  hobbies: ["Reading", "Fishing", "Camping"],
+  placesLived: [
+    {
+      place: "Rexburg, ID",
+      length: "5 years",
+    },
+    {
+      place: "Ammon, ID",
+      length: "3 years",
+    },
+    {
+      place: "Sandy, UT",
+      length: "1 year",
+    },
+  ],
+};
+const foodsElement = document.querySelector("#favorite-foods");
+const placesElement = document.querySelector("#places-lived");
+// requires a list, and a callback that will produce an html string for each item in the list
+// returns a string of html
+function generateListMarkup(list, templateCallback) {
+  const htmlList = list.map(templateCallback);
+  return htmlList.join("");
+}
+// requires an food string
+// returns that string embedded in HTML markup
+function foodsTemplate(food) {
+  return `<li>${food}</li>`;
+}
+
+// requires an place string
+// returns that string embedded in HTML markup
+function placesTemplate(place) {
+  return `<dt>${place.place}</dt><dd>${place.length}</dd>`;
+}
+
+foodsElement.innerHTML = generateListMarkup(
+  myInfo.favoriteFoods,
+  foodsTemplate
+);
+placesElement.innerHTML = generateListMarkup(
+  myInfo.placesLived,
+  placesTemplate
+);
--- week06/scripts/loops.js
+++ week06/scripts/loops.js
@@ -34,22 +34,24 @@ myInfo = {
     favoriteFood2.textContent = food;
     return favoriteFood2;
   }
-  myInfo.favoriteFoods.map(groupFoodItem); 
+  myInfo.favoriteFoods.map(groupFoodItem);
+
+  
 
   /*
   // Step 4: For each favorite food in the favoriteFoods property, create an HTML <li> element and place its value in the <li> element
   let favoriteFood1 = document.createElement("li");
   favoriteFood1.textContent = myInfo.favoriteFoods[0];
-  
+
   let favoriteFood2 = document.createElement("li");
   favoriteFood2.textContent = myInfo.favoriteFoods[1];
-  
+
   let favoriteFood3 = document.createElement("li");
   favoriteFood3.textContent = myInfo.favoriteFoods[2];
-  
+
   let favoriteFood4 = document.createElement("li");
   favoriteFood4.textContent = myInfo.favoriteFoods[3];
-  
+
   // Step 5: Append the <li> elements created above as children of the HTML <ul> element with an ID of favorite-foods
   document.querySelector("#favorite-foods").appendChild(favoriteFood1);
   document.querySelector("#favorite-foods").appendChild(favoriteFood2);
